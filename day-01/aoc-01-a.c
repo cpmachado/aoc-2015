2bbc57b4949104351d7aa918743834ebd793c892
cpmachado <cpmachado@protonmail.com>
2022-12-04 14:33:58 +0000

    Solution for day 01 a

--- day-01/aoc-01-a.c
+++ day-01/aoc-01-a.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
 
 int
+evaluate(char *s) {
+	int parity = 0;
+
+	while (*s) {
+		if (*s == '(') {
+			parity += 1;
+		} else if (*s == ')') {
+			parity -= 1;
+		}
+		s++;
+	}
+
+	return parity;
+}
+
+int
 main(void) {
-	printf("Advent Of Code ftw!!\n");
+	char buffer[BUFSIZ];
+	int acc = 0;
+
+	while(fgets(buffer, BUFSIZ, stdin)) {
+		acc += evaluate(buffer);
+	}
+
+	printf("%d\n", acc);
 	return 0;
 }
